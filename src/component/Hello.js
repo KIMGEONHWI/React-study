fe6eb9e121c1ddfa278cdbd8ad47734b0d55eb6f
김건휘 <66071954+KIMGEONHWI@users.noreply.github.com>
2024-03-19 18:23:03 +0900

    commit

---
+++ src/component/Hello.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export default function Hello() {
+  // let name = "Mike"
+  const [name, setName] = useState("Mike");
+
+function changeName() {
+  setName(name === "Mike"? "Jane" : "Mike"); 
+}
+  return (
+      <div>
+          <h2 id="name">{name}</h2>
+          <button onClick={changeName}>Change</button>
+      </div>
+  );
+}
+
